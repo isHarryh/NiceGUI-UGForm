8eb92766cd0aaae2b5c8a603cd165b98e49682d3
Harry Huang <harryhuang2652@qq.com>
2025-12-25 20:49:09 +0800

    feat: first version

--- nicegui_ugform/__init__.py
+++ nicegui_ugform/__init__.py
@@ -1,1 +1,26 @@
 """NiceGUI-UGForm: A form builder and display library for NiceGUI."""
+
+from .core import (
+    BaseFormField,
+    BaseFormNode,
+    BooleanField,
+    FloatField,
+    Form,
+    IntegerField,
+    TextField,
+)
+from .ui import FormDisplay, FormEditor
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "BaseFormNode",
+    "BaseFormField",
+    "TextField",
+    "FloatField",
+    "IntegerField",
+    "BooleanField",
+    "Form",
+    "FormEditor",
+    "FormDisplay",
+]
